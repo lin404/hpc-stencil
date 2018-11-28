1a57416f75f717d018092389fda8926ff87c0925
Lin <lin.feng.feeling@gmail.com>
2018-11-28 12:42:02 +0000

    modify 2D

--- src/stencil_2d.c
+++ src/stencil_2d.c
@@ -7,12 +7,12 @@
 #define OUTPUT_FILE "stencil.pgm"
 #define MIN(a,b) (((a)<(b))?(a):(b))
 
-void stencil(const int nx, const int ny, double **  image, double ** tmp_image);
-void init_image(const int nx, const int ny, double**  image);
-void output_image(const char * file_name, const int nx, const int ny, double **image);
+void stencil(const int nx, const int ny, float **  image, float ** tmp_image);
+void init_image(const int nx, const int ny, float**  image);
+void output_image(const char * file_name, const int nx, const int ny, float **image);
 double wtime(void);
-double** matrixAllocate(int x, int y);
-void matrixFree(double** matrix, int size);
+float** matrixAllocate(int x, int y);
+void matrixFree(float** matrix, int size);
 
 int main(int argc, char *argv[]) {
 
@@ -28,8 +28,8 @@ int main(int argc, char *argv[]) {
   int niters = atoi(argv[3]);
 
   // Allocate the image
-  double **image = matrixAllocate(nx, ny);
-  double **tmp_image = matrixAllocate(nx, ny);
+  float **image = matrixAllocate(nx, ny);
+  float **tmp_image = matrixAllocate(nx, ny);
 
   // Set the input image
   init_image(nx, ny, image);
@@ -54,80 +54,74 @@ int main(int argc, char *argv[]) {
   matrixFree(tmp_image, ny);
 }
 
-// 00 01 02 
-// 10 11 12
-// 20 21 22
-// 30 31 32
+void stencil(const int nx, const int ny, float ** restrict image, float ** restrict tmp_image) {
 
-void stencil(const int nx, const int ny, double ** restrict image, double ** restrict tmp_image) {
-
-// #pragma GCC ivdep
-for (int y = 1; y < ny-1; y+=3) {
-  // #pragma GCC ivdep
-for (int x = 1; x < nx-1; x+=3) {
-  // #pragma GCC ivdep
-  for (int j = y; j < MIN(y+3,ny-1); ++j) {
-    // #pragma GCC ivdep
-    for (int i = x; i < MIN(x+3,nx-1); ++i) {
-      tmp_image[j][i] = image[j][i] * 3.0/5.0
-        + image[j-1][i] * 0.5/5.0 // TOP
-        + image[j+1][i] * 0.5/5.0 // BELOW
-        + image[j][i-1] * 0.5/5.0 // LEFT
-        + image[j][i+1] * 0.5/5.0; // RIGHT
+#pragma ivdep
+for (int y = 1; y < ny-1; y+=1024) {
+#pragma ivdep
+for (int x = 1; x < nx-1; x+=1024) {
+  #pragma ivdep
+  for (int j = y; j < MIN(y+1024,ny-1); ++j) {
+    for (int i = x; i < MIN(x+1024,nx-1); ++i) {
+      tmp_image[j][i] = image[j][i] * 3.0f/5.0f
+        + image[j-1][i] * 0.5f/5.0f // TOP
+        + image[j+1][i] * 0.5f/5.0f // BELOW
+        + image[j][i-1] * 0.5f/5.0f // LEFT
+        + image[j][i+1] * 0.5f/5.0f; // RIGHT
     }
   }
     }}
 
-#pragma GCC ivdep
+#pragma ivdep
   for (int i = 1; i < nx-1; ++i) {
     // FIRST row
-    tmp_image[0][i] = image[0][i] * 3.0/5.0
-      + image[1][i] * 0.5/5.0 // BELOW
-      + image[0][i-1] * 0.5/5.0 // LEFT
-      + image[0][i+1] * 0.5/5.0; // RIGHT
+    tmp_image[0][i] = image[0][i] * 3.0f/5.0f
+      + image[1][i] * 0.5f/5.0f // BELOW
+      + image[0][i-1] * 0.5f/5.0f // LEFT
+      + image[0][i+1] * 0.5f/5.0f; // RIGHT
     // LAST row
-    tmp_image[ny-1][i] = image[ny-1][i] * 3.0/5.0
-      + image[ny-2][i] * 0.5/5.0 // TOP
-      + image[ny-1][i-1] * 0.5/5.0 // LEFT
-      + image[ny-1][i+1] * 0.5/5.0; // RIGHT
+    tmp_image[ny-1][i] = image[ny-1][i] * 3.0f/5.0f
+      + image[ny-2][i] * 0.5f/5.0f // TOP
+      + image[ny-1][i-1] * 0.5f/5.0f // LEFT
+      + image[ny-1][i+1] * 0.5f/5.0f; // RIGHT
   }
 
-#pragma GCC ivdep
+#pragma ivdep
   for (int i = 1; i < ny-1; ++i) {
     // FIRST column
-    tmp_image[i][0] = image[i][0] * 3.0/5.0
-      + image[i-1][0] * 0.5/5.0 // TOP
-      + image[i+1][0] * 0.5/5.0 // BELOW
-      + image[i][1] * 0.5/5.0; // RIGHT
+    tmp_image[i][0] = image[i][0] * 3.0f/5.0f
+      + image[i-1][0] * 0.5f/5.0f // TOP
+      + image[i+1][0] * 0.5f/5.0f // BELOW
+      + image[i][1] * 0.5f/5.0f; // RIGHT
     // LAST column
-    tmp_image[i][nx-1] = image[i][nx-1] * 3.0/5.0
-      + image[i-1][nx-1] * 0.5/5.0 // TOP
-      + image[i+1][nx-1] * 0.5/5.0 // BELOW
-      + image[i][nx-2] * 0.5/5.0; // LEFT
+    tmp_image[i][nx-1] = image[i][nx-1] * 3.0f/5.0f
+      + image[i-1][nx-1] * 0.5f/5.0f // TOP
+      + image[i+1][nx-1] * 0.5f/5.0f // BELOW
+      + image[i][nx-2] * 0.5f/5.0f; // LEFT
   }
   // TOP-LEFT corner
-  tmp_image[0][0] = image[0][0] * 3.0/5.0
-    + image[1][0] * 0.5/5.0 // BELOW
-    + image[0][1] * 0.5/5.0; // RIGHT
+  tmp_image[0][0] = image[0][0] * 3.0f/5.0f
+    + image[1][0] * 0.5f/5.0f // BELOW
+    + image[0][1] * 0.5f/5.0f; // RIGHT
   
   // TOP-RIGHT corner
-  tmp_image[0][nx-1] = image[0][nx-1] * 3.0/5.0
-    + image[1][nx-1] * 0.5/5.0 // BELOW
-    + image[0][nx-2] * 0.5/5.0; // LEFT
+  tmp_image[0][nx-1] = image[0][nx-1] * 3.0f/5.0f
+    + image[1][nx-1] * 0.5f/5.0f // BELOW
+    + image[0][nx-2] * 0.5f/5.0f; // LEFT
   
   // BELOW-LEFT corner
-  tmp_image[ny-1][0] = image[ny-1][0] * 3.0/5.0
-    + image[ny-2][0] * 0.5/5.0 // TOP
-    + image[ny-1][1] * 0.5/5.0; // RIGHT
+  tmp_image[ny-1][0] = image[ny-1][0] * 3.0f/5.0f
+    + image[ny-2][0] * 0.5f/5.0f // TOP
+    + image[ny-1][1] * 0.5f/5.0f; // RIGHT
   
   // BELOW-RIGHT corner
-  tmp_image[ny-1][nx-1] = image[ny-1][nx-1] * 3.0/5.0
-    + image[ny-2][nx-1] * 0.5/5.0 // TOP
-    + image[ny-1][nx-2] * 0.5/5.0; // LEFT
+  tmp_image[ny-1][nx-1] = image[ny-1][nx-1] * 3.0f/5.0f
+    + image[ny-2][nx-1] * 0.5f/5.0f // TOP
+    + image[ny-1][nx-2] * 0.5f/5.0f; // LEFT
 }
 
 // Create the input image
-void init_image(const int nx, const int ny, double** image) {
+void init_image(const int nx, const int ny, float ** image) {
   for (int j = 0; j < ny; ++j) {
     for (int i = 0; i < nx; ++i) {
       if (((i/(nx/8))%2) == ((j/(nx/8))%2)) {
@@ -140,7 +134,7 @@ void init_image(const int nx, const int ny, double** image) {
 }
 
 // Routine to output the image in Netpbm grayscale binary image format
-void output_image(const char * file_name, const int nx, const int ny, double **image) {
+void output_image(const char * file_name, const int nx, const int ny, float **image) {
 
   // Open output file
   FILE *fp = fopen(file_name, "w");
@@ -182,16 +176,16 @@ double wtime(void) {
   return tv.tv_sec + tv.tv_usec*1e-6;
 }
 
-double** matrixAllocate(int x, int y) {
- double** matrix = malloc(y*sizeof(double*));
+float** matrixAllocate(int x, int y) {
+ float** matrix = malloc(y*sizeof(float*));
 
  for (int j=0; j<y; j++) {
-  matrix[j] = malloc(x*sizeof(double));
+  matrix[j] = malloc(x*sizeof(float));
  }
 
  return matrix;
 }
-void matrixFree(double** matrix, int size) {
+void matrixFree(float** matrix, int size) {
   for(int i =0; i<size; i++) {
     free(matrix[i]);
   }
